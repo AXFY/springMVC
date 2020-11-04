2b32577c48d11f2958a61b34e55c7a4169a625f1
zhoulonghui <http://171.221.254.176:23980/cqns-poc/cros-cas-project.git>
2020-11-04 18:55:01 +0800

    222

---
+++ src/main/java/bean/Student.java
@@ -0,0 +1,26 @@
+package bean;
+
+/**
+ * @author AXFY
+ * @date 2020/11/4 18:46
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -1,8 +1,15 @@
 package controller;
 
+import bean.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.annotation.Resource;
 
 /**
  * @author AXFY
@@ -10,8 +17,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class MyController {
+
+    @Autowired()
+    @Qualifier("student")
+    private Student test;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping(value = "/ridict", method = RequestMethod.GET)
+    public String ridict(){
+        return "ridict";
+    }
+
+    @RequestMapping(value = "/text")
+    public ModelAndView text(){
+        return new ModelAndView((new RedirectView("../hello.jsp")));
     }
 }
